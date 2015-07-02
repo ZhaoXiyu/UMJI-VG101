cd97b5467a15a0af29189f253834fcdd4bbc4737
tripack <tripack@126.com>
2015-07-02 15:16:23 +0800

    A new implementation of resize. More Robust

--- HMWK/exp/expr.c
+++ HMWK/exp/expr.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 int uround(float,float);
+
 int main(){
+	FILE* =fopen("data.in","r");
+	
 	char* a="Hello";
 	printf("%c",);
 	return 0;
+	
 }
 
 int uround(float a,float b){return (int)(a+b+1.5);}
--- HMWK/hw6/ex_5.c
+++ HMWK/hw6/ex_5.c
@@ -6,20 +6,22 @@
 #define TYPE (set->type)
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void* umalloc(size_t);
 void* urealloc(void*,size_t);
 int bcmp(unsigned char*,unsigned char*,size_t);
+void resizeSet(uset*,int);
 
 int main(){
 	uset intset;
-	newSet(&intset,INT);
+	newSet(&intset,CHAR);
 	int i;
-	for(i=0;i<20000;i++){
+	for(i=0;i<2000;i++){
 		//printf("%d %d \n",i,intset.card);
 		addElem(&i,&intset);
 	}
-	for(i=25000;i>15000;i--){
+	for(i=2500;i>1500;i--){
 		//printf("%d %d \n",i,intset.card);
 		remElem(&i,&intset);
 	}
@@ -29,27 +31,27 @@ int main(){
 
 void addElem(void *elem, uset *set){
 	byte* i;
-	for(i=ELEM;(i-ELEM)<CARD;i+=TYPE)
+	int currsize=CARD*TYPE;
+	for(i=ELEM;(i-ELEM)<currsize;i+=TYPE)
 		if(bcmp(elem,i,TYPE))return;//Same element already exist
-	//printf("%d %d\n",*(int*)elem,CARD);
-	if(((CARD-INITSETSIZE*TYPE)%MCHNK)==0&&CARD>=INITSETSIZE*TYPE)//if the space is critical.
-		set->elem=urealloc(ELEM,CARD+MCHNK);//Resize to 64 more.
-	memcpy(ELEM+CARD,elem,TYPE);//add to the set.
-		//source,start byte+card,length
-	CARD+=TYPE;//inc card by byte
+	printf("Adding:%d %d\n",*(int*)elem,CARD);
+	resizeSet(set,currsize+TYPE);//resize the set to hold another elem
+	memcpy(ELEM+currsize,elem,TYPE);//add to the set.
+		//source,start byte+size,length
+	CARD++;//inc card by 1
 	return;
 }
 
 void remElem(void *elem, uset *set){
 	byte* i;
-	//printf("%d\n",*(int*)elem);
-	for(i=ELEM;(i-ELEM)<CARD;i+=TYPE)
+	printf("Removing:%d\n",*(int*)elem);
+	int currsize=CARD*TYPE;
+	for(i=ELEM;(i-ELEM)<currsize;i+=TYPE)
 		if(bcmp(elem,i,TYPE)){
-			memmove(i,i+TYPE,CARD-(i-ELEM)-TYPE+1);//Push all memory forward type bytes
-			CARD-=TYPE;//dec card.
-			if((CARD>INITSETSIZE*TYPE)
-				&&((CARD-(INITSETSIZE*TYPE))%MCHNK))
-					set->elem=realloc(ELEM,CARD);
+			memmove(i,i+TYPE,currsize-(i-ELEM)-TYPE+1);
+			//Push all memory forward type bytes
+			resizeSet(set,currsize-TYPE);//Resize to 1 elem less
+			CARD--;//dec 1.
 			return;//finished processing
 		}
 	return; //Not found,nothing to do.
@@ -94,4 +96,19 @@ int bcmp(unsigned char* ptr1,unsigned char* ptr2,size_t size){
 		if(ptr1[size]!=ptr2[size])return 0;//some different;
 	}
 	return 1;
+}
+
+void resizeSet(uset* set,int newsize){
+	int oldsize=set->card*set->type;//calculate old used mem
+	int t=oldsize%(MCHNK*set->type);//calculate the used mem in the last chunk
+	oldsize=oldsize-t+MCHNK*set->type;
+	//calculate the mem allocated by calculating (orignal size-t+chunksize)
+	t=newsize%(MCHNK*set->type);
+	newsize=newsize-t+MCHNK*set->type;
+	//calculate the new memory required to be allocated;
+	if(oldsize!=newsize){//if newsize is not equal to old size.
+		set->elem=urealloc(set->elem,newsize);//resize memory!
+		printf("Resizing from %d to %d.\n",oldsize,newsize);
+	}
+	return;
 }
